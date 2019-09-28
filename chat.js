e9ec39a5eca3027bdc03fde34b34b34c8fa805b4
Chris Lenart <clenart4@kent.edu>
2019-09-28 18:04:40 -0400

    removed homepage chat

--- chat.js
+++ chat.js
@@ -1,9 +1,11 @@
-document.getElementsByClassName("live-chat-accord")[0].addEventListener("click", function() {
-	this.classList.toggle("active-live-chat");
-	var panel = this.nextElementSibling;
-	if (panel.style.display === "block") {
-		panel.style.display = "none";
-	} else {
-		panel.style.display = "block";
-	}
-});
+if (v) {
+	document.getElementsByClassName("live-chat-accord")[0].addEventListener("click", function() {
+		this.classList.toggle("active-live-chat");
+		var panel = this.nextElementSibling;
+		if (panel.style.display === "block") {
+			panel.style.display = "none";
+		} else {
+			panel.style.display = "block";
+		}
+	});
+}
--- content.js
+++ content.js
@@ -1,18 +1,19 @@
 const url = new URL(window.location);
 const params = new URLSearchParams(url.search);
 const v = params.get('v');
-const chat = document.createElement('div');
-chat.style.cssText = 'position:fixed;bottom:0;right:0;z-index:9999;';
-chat.innerHTML = `
-<div class="live-chat">
-  <button class="live-chat-accord">Current Users 2</button>
-  <div class="panel-live-chat">
-    <input class="live-sub-chat" type="text">
-    <form class="livechat-sub">
-      <input class="live-sub-mess" type="text" placeholder="Message">
-      <input class="live-sub-button" type="submit" value="Submit">
-    </form>
-  </div>
-</div>
-`;
-document.body.appendChild(chat);
+if (v) {
+	const chat = document.createElement('div');
+	chat.innerHTML = `
+	<div class="live-chat">
+	  <button class="live-chat-accord">Current Users 2</button>
+	  <div class="panel-live-chat">
+		<input class="live-sub-chat" type="text">
+		<form class="livechat-sub">
+		  <input class="live-sub-mess" type="text" placeholder="Message">
+		  <input class="live-sub-button" type="submit" value="Submit">
+		</form>
+	  </div>
+	</div>
+	`;
+	document.body.appendChild(chat);
+}
