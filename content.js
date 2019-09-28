0ce79c383dc5e16711b7c83c726708ab5cf73528
Chris Lenart <clenart4@kent.edu>
2019-09-28 12:31:58 -0400

    added function to wait on load

--- content.js
+++ content.js
@@ -1,6 +1,8 @@
-var video_id = window.location.search.split('v=')[1];
-var ampersandPosition = video_id.indexOf('&');
+{
+	var video_id = window.location.search.split('v=')[1];
+	var ampersandPosition = video_id.indexOf('&');
 
-if(ampersandPosition != -1) {
-  video_id = video_id.substring(0, ampersandPosition);
-}
+	if(ampersandPosition != -1) {
+	  video_id = video_id.substring(0, ampersandPosition);
+	}
+}{}
